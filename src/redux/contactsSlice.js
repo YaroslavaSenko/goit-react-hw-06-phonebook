864f0c662604ba5cd79b28d74cad54f4cbc7eb78
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-11 15:26:42 +0300

    Create contactsSlice.js

---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+export const contactsSlice = createSlice({
+  name: 'phonebook',
+  initialState: {
+    contacts: [],
+  },
+  reducers: {
+    addContact(state, action) {
+      state.contacts.push(action.payload);
+    },
+    deleteContact(state, action) {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
+
+const persistConfig = {
+  key: 'phonebook',
+  storage,
+};
+
+export const contactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
+export const { addContact, deleteContact } = contactsSlice.actions;
