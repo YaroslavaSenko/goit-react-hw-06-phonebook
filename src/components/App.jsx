7c8825df17e9a421316f1bce60ab608758376bf9
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-18 18:07:11 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,22 @@
 // import { useEffect, useState } from "react";
-import ContactForm from "./ContactForm/ContactForm";
-import Filter from "./Filter/Filter";
-import ContactList from "./ContactList/ContactList";
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
 // import { nanoid } from 'nanoid';
 import { Contacts } from './App.styled';
 
 export const App = () => {
   return (
     <Contacts>
-  <h1>Phonebook</h1>
-  <ContactForm/>
+      <h1>Phonebook</h1>
+    
 
-  <h2>Contacts</h2>
-  <Filter/>
+      <ContactForm />
 
-  <ContactList/>
-</Contacts>
+      <h2>Contacts</h2>
+      <Filter />
+
+      <ContactList />
+    </Contacts>
   );
- }
-
-
+};
