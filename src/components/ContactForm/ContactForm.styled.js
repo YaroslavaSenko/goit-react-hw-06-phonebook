17900b34224f514261f0c9975bae86fba37e69d5
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-18 18:07:23 +0300

    Update ContactForm.styled.js

--- src/components/ContactForm/ContactForm.styled.js
+++ src/components/ContactForm/ContactForm.styled.js
@@ -1,23 +1,19 @@
 import styled from '@emotion/styled';
-import {Field} from 'formik';
-import {Form as FormikForm} from 'formik';
 
-
-export const Form = styled(FormikForm)`
+export const Form = styled.form`
 display: block;
 margin-top: 20px;
 margin-bottom: 20px;
 `;
 
-export const Input = styled(Field)`
-height: auto;
+export const FormInput = styled.input`
 padding: 10px;
 background-color: white;
 border: 1px solid #d8e4e2;
 border-radius: 8px;
 `;
 
-export const Button = styled.button`
+export const SubmitButton = styled.button`
 padding: 8px 15px;
 margin: 10px;
 font-size: 12px;
@@ -29,3 +25,10 @@ background-color: #4CAF50;
 border: none;
 border-radius: 15px;
 `;
+
+
+export const FormLabel = styled.label`
+  margin-right: 15px;
+`;
+
+
